412d27c0664d8ac6768721a4d375a5009f8a8d31
Yuri Sodré <89286829+ySodre@users.noreply.github.com>
2025-09-02 07:24:18 -0300

    Atualizacao

--- src/main.go
+++ src/main.go
@@ -9,7 +9,7 @@ import (
 func index(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintf(w, "Aplicacao exemplo 5.0")
+	fmt.Fprintf(w, "Aplicacao exemplo 6.0")
 }
 
 func main() {
